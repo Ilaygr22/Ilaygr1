3e7fc0abaecfa2ef18c066c3087046081b5f7eab
Ilaygr <ilay.grundland@gmail.com>
2026-09-14 17:58:10 +0300

    Network

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
+import argparse
 import socket
 
 
-def run_server (ip, port):
-    
+def run_server(ip, port):
+
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((ip, port))
     server.listen(5)
@@ -11,9 +12,18 @@ def run_server (ip, port):
     with conn:
         print(f"connection by {addr}")
         while True:
-            data = conn.recv(1024) 
+            data = conn.recv(1024)
             if not data:
                 break
             print("Received message:", data.decode("utf-8"))
 
-run_server('127.0.0.1', 65432)
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Recieve data from client")
+    parser.add_argument("port", type=int, help="the server's port")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = get_args()
+    run_server("127.0.0.1", args.port)
