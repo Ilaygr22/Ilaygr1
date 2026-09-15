30b6a0bf68ad4a5ebfd1a0f86b040e7996dadcf1
Ilaygr <ilay.grundland@gmail.com>
2026-09-15 14:36:18 +0300

    Networking1

--- server.py
+++ server.py
@@ -1,29 +1,40 @@
 import argparse
 import socket
+from threading import Thread
 
 
 def run_server(ip, port):
 
+    # listen for connection, and then infinitly create threads for any connection made
+
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((ip, port))
     server.listen(5)
+    while True:
+        conn, addr = server.accept()
+        t = Thread(target=handle_connection, args=(conn, addr))
+        t.run()
 
-    conn, addr = server.accept()
+
+def handle_connection(conn, addr):
+
+    # open connection to conn, and receive data and data length
     with conn:
         print(f"connection by {addr}")
         while True:
             data = conn.recv(1024)
             if not data:
                 break
-            print("Received message:", data.decode("utf-8"))
+            print("Received message:", data.decode("utf-8"), data)
 
 
 def get_args():
     parser = argparse.ArgumentParser(description="Recieve data from client")
+    parser.add_argument("ip", type=str, help="the server's ip")
     parser.add_argument("port", type=int, help="the server's port")
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = get_args()
-    run_server("127.0.0.1", args.port)
+    run_server(args.ip, args.port)
